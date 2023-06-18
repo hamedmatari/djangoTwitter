a93f0407d02ce0bf569422f50f7e75769b5f7e9d
hamed.m <hamed.m@arianalabs.io>
2023-06-18 12:32:29 +0330

    FEAT: add tweet model

--- tweets/models.py
+++ tweets/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Tweet(models.Model):
+    content = models.CharField(max_length=280)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.content[:50]  # Return a truncated version of the tweet content
+
+    class Meta:
+        ordering = ["-timestamp"]
