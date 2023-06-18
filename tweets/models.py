29210e329cd3ed34affb40613a6a8dcbb8c2ff64
Your Name <matari.hamed@yahoo.com>
2023-06-18 21:55:20 +0330

    ADD: like_users to tweet model

--- tweets/models.py
+++ tweets/models.py
@@ -6,6 +6,9 @@ class Tweet(models.Model):
     content = models.CharField(max_length=280)
     timestamp = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(TwitterUser, on_delete=models.CASCADE)
+    liked_users = models.ManyToManyField(
+        TwitterUser, related_name="liked_tweets", blank=True
+    )
 
     def __str__(self):
         return self.content[:50]  # Return a truncated version of the tweet content
