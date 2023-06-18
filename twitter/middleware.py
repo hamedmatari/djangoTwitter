8d7f660b20aa13a4761f069d1ab57549ca9ac907
hamed.m <hamed.m@arianalabs.io>
2023-06-18 15:40:57 +0330

    FEAT: check authentication with auth header

--- twitter/middleware.py
+++ twitter/middleware.py
@@ -1,13 +1,23 @@
 from django.core.exceptions import PermissionDenied
+from django.core.cache import cache
+from django.contrib.auth import login
 
 
 def simple_middleware(get_response):
     # One-time configuration and initialization.
 
     def middleware(request):
-        if "auth" in request.headers and request.headers["auth"] == "manHamedHastam":
+        if request.path_info.startswith("/login/"):
             response = get_response(request)
             return response
+
+        # if "auth" in request.headers and request.headers["auth"] == "manHamedHastam":
+        if "auth" in request.headers:
+            username = cache.get(request.headers["auth"])
+            if username is not None:
+                request.user = username
+                response = get_response(request)
+                return response
 
         raise PermissionDenied()
 
