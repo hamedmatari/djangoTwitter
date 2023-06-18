6f06666643e6f6bf8e9038ce244af877aea4212f
hamed.m <hamed.m@arianalabs.io>
2023-06-18 17:43:06 +0330

    FEAT: logout view

--- twitter/views.py
+++ twitter/views.py
@@ -4,7 +4,6 @@ from django.core.cache import cache
 from django.views.decorators.csrf import csrf_exempt
 
 
-@csrf_exempt
 def login_view(request):
     if request.method == "POST":
         print("in log in view")
@@ -17,13 +16,20 @@ def login_view(request):
         if user is not None:
             # Generate a token
             token = generate_token()
-            cache.set(token, username)
+            cache.set(token, username, 1800)  # 30 minutes
 
             # Return the token as a JSON response
             return JsonResponse({"token": token})
 
     # Invalid credentials or GET request
     return JsonResponse({"error": "Invalid credentials"}, status=401)
+
+
+def logout_view(request):
+    if request.method == "POST":
+        cache.delete(request.headers["auth"])
+        return JsonResponse({"result": "success"})
+    return JsonResponse({"error": "somethings went wrong"})
 
 
 def generate_token():
